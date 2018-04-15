11ba42b560286722f882ce05cbcbc29444a9bab7
Alice Creasey <alice.creasey@hotmail.com>
2018-04-15 14:42:38 +0100

    Just tidied up

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -4,15 +4,8 @@ class UserController {
     public function login() {
       // we expect a url of form ?controller=products&action=login
       // if it's a GET request display a blank form for the user to login
-      // else they are logged in already so we display all posts ???? Not sure if this logic is sound!
         if($_SERVER['REQUEST_METHOD'] == 'GET'){
           require_once('views/pages/login.php');
-      }
-      else { 
-            Post::add();
-             
-            $posts = Post::all(); //$products is used within the view
-            require_once('views/posts/readAll.php');
-      }
+        }
     }
 }
--- views/pages/login.php
+++ views/pages/login.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <h1>Login!</h1>
-        <form>
+        <form action="" method="POST" class="w3-container" enctype="multipart/form-data">
             <p>
                 <input class="w3-input" type="email" name="email" required autofocus>
                 <label>Email address</label>
@@ -21,8 +21,5 @@
             </p>
             
         </form>
-        <?php
-        // put your code here
-        ?>
     </body>
 </html>
